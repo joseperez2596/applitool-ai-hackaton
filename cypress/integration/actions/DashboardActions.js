c561ff5f5f20f6573b3d219c3eab7a91c23bb13b
joseperez2596 <joseperez2599@gmail.com>
2019-11-20 23:49:26 -0400

    Add Table Sort Test

--- cypress/integration/actions/DashboardActions.js
+++ cypress/integration/actions/DashboardActions.js
@@ -1,4 +1,5 @@
 import * as DashboardPage from '../elements/DashboardPageElements';
+import * as DashboardData from '../data/DashboardData';
 
 export function checkDashboard() {
   cy.get(DashboardPage.DashboardPageElements.UserPicture)
@@ -7,4 +8,28 @@ export function checkDashboard() {
 
 export function clickCompareExpensesLink() {
   cy.get(DashboardPage.DashboardPageElements.CompareExpensesLink).click();
+}
+
+export function clickAmountsHeader() {
+  cy.get(DashboardPage.DashboardPageElements.AmountsHeader).click();
+}
+
+export function checkAmountsOrdering() {
+  var i = 1;
+  Object.entries(DashboardData.DashboardPageData.Rows)
+    .forEach(([, value]) => {
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td:nth-child(1) > span:nth-child(2)`)
+      .should('have.text', value.Status);
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td:nth-child(2) > span:nth-child(1)`)
+      .should('have.text', value.Date);
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td:nth-child(2) > span.smaller.lighter`)
+      .should('have.text', value.Time);
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td.cell-with-media > span`)
+      .should('have.text', value.Description);
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td.text-center > a`)
+      .should('have.text', value.Category);
+    cy.get(`#transactionsTable > tbody > tr:nth-child(${i}) > td.text-right.bolder.nowrap > span`)
+      .should('have.text', value.Amount);
+    i++;
+  });
 }
---
+++ cypress/integration/data/DashboardData.js
@@ -0,0 +1,54 @@
+const Rows = {
+  Row1: {
+    Status: 'Pending',
+    Date: 'Yesterday',
+    Time: '7:45am',
+    Description: 'MailChimp Services',
+    Category: 'Software',
+    Amount: '- 320.00 USD',
+  },
+  Row2: {
+    Status: 'Complete',
+    Date: 'Jan 7th',
+    Time: '9:51am',
+    Description: 'Ebay Marketplace',
+    Category: 'Ecommerce',
+    Amount: '- 244.00 USD',
+  },
+  Row3: {
+    Status: 'Pending',
+    Date: 'Jan 23rd',
+    Time: '2:7pm',
+    Description: 'Shopify product',
+    Category: 'Shopping',
+    Amount: '+ 17.99 USD',
+  },
+  Row4: {
+    Status: 'Pending',
+    Date: 'Jan 9th',
+    Time: '7:45pm',
+    Description: ' Templates Inc',
+    Category: 'Business',
+    Amount: '+ 340.00 USD',
+  },
+  Row5: {
+    Status: 'Declined',
+    Date: 'Jan 19th',
+    Time: '3:22pm',
+    Description: 'Stripe Payment Processing',
+    Category: 'Finance',
+    Amount: '+ 952.23 USD',
+  },
+  Row6: {
+    Status: 'Complete',
+    Date: 'Today',
+    Time: '1:52am',
+    Description: 'Starbucks coffee',
+    Category: 'Restaurant / Cafe',
+    Amount: '+ 1,250.00 USD',
+  },
+};
+
+export const DashboardPageData = {
+  Rows,
+};
